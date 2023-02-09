ac9f1844774359b43ee82d1a65fcb15e20c36dc8
Andrea Fernández <amferzeb@gmail.com>
2023-02-09 01:04:03 -0300

    Primer escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,9 @@
+
+  
+document.getElementById('boton').onclick = function (params) {
+  
+    console.log ("Capturamos el evento click");
+    document.getElementById ("demo"). innerHTML ="Estamos probando nuestro primer evento en JS";
+
+}  
+  
