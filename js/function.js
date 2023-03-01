b9367acd96a84d02a93832271302d49353c48f75
AndreaFerzeb <124212025+AndreaFerzeb@users.noreply.github.com>
2023-03-01 20:56:26 -0300

    Delete function.js

--- js/function.js
+++
@@ -1,9 +0,0 @@
-
-  
-document.getElementById('boton').onclick = function (params) {
-  
-    console.log ("Capturamos el evento click");
-    document.getElementById ("demo"). innerHTML ="Estamos probando nuestro primer evento en JS";
-
-}  
-  
